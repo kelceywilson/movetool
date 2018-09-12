7ea068ebcb705e0f6b6e9be58795838586358807
Kelcey Wilson <kelceywilson@gmail.com>
2018-09-12 13:59:13 -0700

    Fix resume for deployment situation

--- client/src/components/profiles/Resume.js
+++ client/src/components/profiles/Resume.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
-// import Resume from "../../img/Kelcey-Wilson-Resume.pdf";
+import resume from "../../img/resume.pdf";
 
 export default class Resume extends Component {
   render() {
     return (
       <div className="fluidMedia">
-        <iframe
-          title="Resume"
-          src="./resume.pdf"
-          width="100%"
-          height="100%"
-          download
-        >
+        <iframe title="Resume" src={resume} width="100%" height="100%" download>
           <p>Your browser does not support iframes.</p>
         </iframe>
       </div>
