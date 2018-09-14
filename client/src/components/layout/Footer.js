3046b8ccc7e5c2162b83ae9296ddd24e7af79d6f
Kelcey Wilson <kelceywilson@gmail.com>
2018-09-13 20:44:38 -0700

    Reduce copyright font-size

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => {
   return (
-    <footer className="footer">
+    <footer className="mt-5 p-4 text-center footer">
       Copyright &copy; {new Date().getFullYear()} Kelcey Wilson
     </footer>
   );
